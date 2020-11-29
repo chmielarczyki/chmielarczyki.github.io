8a882723ad0b72f9bc239d01f2e1accbc5824ae0
chmielarczyki <iga.chmielarczyk@gmail.com>
2020-11-29 21:36:06 +0100

    pobierz dane po kliknieciu przycisku jquery

---
+++ zadania-domowe/ajax/27603/js/script.js
@@ -0,0 +1,51 @@
+$(document).ready(function() {
+
+    $('#get-data').click(function() {
+
+    //     // wariant 1
+    //    $.get('https://akademia108.pl/api/ajax/get-post.php')
+    //         .done(function(data) {
+
+    //             // console.log(data);
+
+    //             let pId = $('<p></p>').text(`Post ID: ${data.id}`);
+    //             let pUserId = $('<p></p>').text(`User ID: ${data.usreID}`);
+    //             let pBody = $('<p></p>').text(`Body: ${data.body}`);
+    //             let pTitle = $('<p></p>').text(`Title: ${data.title}`);
+    //             let hr = $('<hr />');
+
+    //             $('body').append(pId);
+    //             $('body').append(pUserId);
+    //             $('body').append(pBody);
+    //             $('body').append(pTitle);
+    //             $('body').append(hr);
+    //         })
+    //         .fail(function(error) {
+    //             console.error(error);
+    //         });
+
+
+         // wariant 2
+       $.getJSON('https://akademia108.pl/api/ajax/get-post.php')
+       .done(function(data) {
+
+           // console.log(data);
+
+           let pId = $('<p></p>').text(`Post ID: ${data.id}`);
+           let pUserId = $('<p></p>').text(`User ID: ${data.usreID}`);
+           let pBody = $('<p></p>').text(`Body: ${data.body}`);
+           let pTitle = $('<p></p>').text(`Title: ${data.title}`);
+           let hr = $('<hr />');
+
+           $('body').append(pId);
+           $('body').append(pUserId);
+           $('body').append(pBody);
+           $('body').append(pTitle);
+           $('body').append(hr);
+       })
+       .fail(function(error) {
+           console.error(error);
+       });
+    });
+
+});
