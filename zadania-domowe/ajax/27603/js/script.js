2a96db8292f0116b1becb2263d4c4d707588591e
chmielarczyki <iga.chmielarczyk@gmail.com>
2020-12-01 20:08:20 +0100

    poprawione data.userId

--- zadania-domowe/ajax/27603/js/script.js
+++ zadania-domowe/ajax/27603/js/script.js
@@ -2,37 +2,37 @@ $(document).ready(function() {
 
     $('#get-data').click(function() {
 
-    //     // wariant 1
-    //    $.get('https://akademia108.pl/api/ajax/get-post.php')
-    //         .done(function(data) {
+        // wariant 1
+       $.get('https://akademia108.pl/api/ajax/get-post.php')
+            .done(function(data) {
 
-    //             // console.log(data);
+                // console.log(data);
 
-    //             let pId = $('<p></p>').text(`Post ID: ${data.id}`);
-    //             let pUserId = $('<p></p>').text(`User ID: ${data.usreID}`);
-    //             let pBody = $('<p></p>').text(`Body: ${data.body}`);
-    //             let pTitle = $('<p></p>').text(`Title: ${data.title}`);
-    //             let hr = $('<hr />');
+                let pId = $('<p></p>').text(`Post ID: ${data.id}`);
+                let pUserId = $('<p></p>').text(`User ID: ${data.userId}`);
+                let pBody = $('<p></p>').text(`Body: ${data.body}`);
+                let pTitle = $('<p></p>').text(`Title: ${data.title}`);
+                let hr = $('<hr />');
 
-    //             $('body').append(pId);
-    //             $('body').append(pUserId);
-    //             $('body').append(pBody);
-    //             $('body').append(pTitle);
-    //             $('body').append(hr);
-    //         })
-    //         .fail(function(error) {
-    //             console.error(error);
-    //         });
+                $('body').append(pId);
+                $('body').append(pUserId);
+                $('body').append(pBody);
+                $('body').append(pTitle);
+                $('body').append(hr);
+            })
+            .fail(function(error) {
+                console.error(error);
+            });
 
 
          // wariant 2
        $.getJSON('https://akademia108.pl/api/ajax/get-post.php')
        .done(function(data) {
 
-           // console.log(data);
+        //    console.log(data);
 
            let pId = $('<p></p>').text(`Post ID: ${data.id}`);
-           let pUserId = $('<p></p>').text(`User ID: ${data.usreID}`);
+           let pUserId = $('<p></p>').text(`User ID: ${data.userId}`);
            let pBody = $('<p></p>').text(`Body: ${data.body}`);
            let pTitle = $('<p></p>').text(`Title: ${data.title}`);
            let hr = $('<hr />');
