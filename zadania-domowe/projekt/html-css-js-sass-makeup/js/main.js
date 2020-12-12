124b49b9c1ff710a669eab40800e2c1eac69e582
chmielarczyki <iga.chmielarczyk@gmail.com>
2020-12-13 00:28:15 +0100

    do team

---
+++ zadania-domowe/projekt/html-css-js-sass-makeup/js/main.js
@@ -0,0 +1,7 @@
+document.getElementsByClassName('nav-hamburger')[0].addEventListener('click', function(){
+    document.getElementsByClassName('open-menu-holder')[0].classList.toggle('open');
+})
+
+document.getElementsByClassName('menu-close')[0].addEventListener('click', function(){
+    document.getElementsByClassName('open-menu-holder')[0].classList.toggle('open');
+})
