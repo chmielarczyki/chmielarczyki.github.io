f0b59e6328d5225c8cd143f6ab4b088e243372ad
chmielarczyki <iga.chmielarczyk@gmail.com>
2020-12-15 17:19:08 +0100

    tworzenie tablicy

--- zadania-domowe/js-dom/27795-dod/js/main.js
+++ zadania-domowe/js-dom/27795-dod/js/main.js
@@ -8,10 +8,15 @@ document.getElementById('oblicz').addEventListener('click', function() {
     let pracownik = document.querySelectorAll('.pracownik');
     console.log(pracownik);
     let podwyzka = 0;
-    let arr = [];
+    let emploeesArray = [];
 
 
     for(i = 0; i < czas.length && i < wyplata.length && i < stawka.length && i < pracownik.length; i++) {
+
+        emploeesArray.push({
+            name: pracownik[i].innerText,
+            workTime: parseFloat(czas[i].value)
+        });
 
         if(czas[i].value > 160) {
             
@@ -38,60 +43,17 @@ document.getElementById('oblicz').addEventListener('click', function() {
             // console.log(pracownik[i]);
         }
 
+        // let person = {
+        //     name: pracownik[i].value,
+        //     time: czas[i].value
+        // }
+
+        // arr.push(person);
+
+        // console.log(emploeesArray.workTime.length);
     }
+    console.log(emploeesArray.workTime);
+
+    
 })
 
-
-// $(document).ready(function(){
-
-    
-// document.getElementById('oblicz').addEventListener('click', function() {
-
-//     let wyplata = document.getElementsByClassName('wyplata');
-//     let czas = document.getElementsByClassName('czas');
-//     let stawka = document.getElementsByClassName('stawka');
-//     let job = document.getElementsByClassName('job');
-//     let pracownik = document.querySelectorAll('.pracownik');
-//     console.log(pracownik);
-//     let podwyzka = 0;
-//     let najCzas = [];
-//     let arr = [];
-    
-
-//     for(i = 0; i < czas.length && i < wyplata.length && i < stawka.length && i < pracownik.length; i++) {
-
-//         if(czas[i].value > 160) {
-            
-//             podwyzka = (czas[i].value- 160)*stawka[i].value*2;
-//             wyplata[i].innerText = (czas[i].value * stawka[i].value)+podwyzka;
-
-//         } else {
-//             wyplata[i].innerText = czas[i].value * stawka[i].value;
-//         }
-
-//         if(czas[i].value < 100) {
-//             pracownik[i].classList.add('red');
-
-//         } else {
-
-//             pracownik[i].classList.add('green');
-
-//             // najCzas += czas[i];
-
-//             // arr.sort();
-
-//             /* nie wiem jak pobrac imiona pracowników zrobić je w tablicę, sort() i slice() pobrac 3 pierwsze elementy */
-//             // document.getElementById('najlepsi-pracownicy').innerHTML = pracownik[i];
-
-//         }
-//         // arr = jQuery.makeArray(najCzas);
-
-//         // arr = jQuery.makeArray( pracownik );
-
-//         // arr.sort();
-//         // console.log(najCzas)
-//         // console.log(arr)
-//     }
-// })
-
-// });
